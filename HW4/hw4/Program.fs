158e288782057200fd2d5f12ae3751a0ae453d1f
Mark G <mark.garcia.8001@gmail.com>
2022-02-28 15:09:15 -0800

    Add Statement, Condition, testCondition and interpret union / fxn

--- HW4/hw4/Program.fs
+++ HW4/hw4/Program.fs
@@ -15,6 +15,7 @@ type Expression =
     | Div of Expression * Expression
     | Sqrt of Expression
     | Pow of Expression * Expression
+    | Input of String
    
 // evaluate converts an Expression object into the floating-point number it represents.
 let rec evaluate expr =
@@ -27,6 +28,7 @@ let rec evaluate expr =
     | Div (expr1, expr2) -> (evaluate expr1) / (evaluate expr2)
     | Sqrt expr1 -> Math.Sqrt (evaluate expr1)
     | Pow (expr1, expr2) -> (evaluate expr1) ** (evaluate expr2)
+    | Input s -> float s 
 
 // format expression
 let rec formatExpression expr =
@@ -39,26 +41,73 @@ let rec formatExpression expr =
     | Div (expr1, expr2) -> sprintf "(%s / %s)" (formatExpression expr1) (formatExpression expr2)
     | Sqrt expr1 -> sprintf "(sqrt(%s))" (formatExpression expr1)
     | Pow (expr1, expr2) -> sprintf "(%s ** %s)" (formatExpression expr1) (formatExpression expr2)
-    
+    | Input s -> sprintf "%.2f" (float s)
 
+type Condition =
+    | Equals of Expression * Expression
+    | And of Condition * Condition
+    | Or of Condition * Condition
+    | Not of Condition
+
+let rec testConditon con =
+    match con with
+    | Equals (expr1, expr2) -> evaluate expr1 = evaluate expr2
+    | And (con1, con2) -> testConditon con1 && testConditon con2
+    | Or (con1, con2) -> testConditon con1 || testConditon con2
+    | Not con1 -> testConditon con1 |> not
+    
+type Statement =
+    | Noop
+    | PrintStr of String
+    | PrintExpr of Expression
+    | Branch of Condition * Statement * Statement
+    | Repeat of int * Statement
+    
+let rec interpret state =
+    match state with
+    | Noop -> ()
+    | PrintStr str -> printfn "%s" str
+    | PrintExpr expr -> evaluate expr |> printfn "%.2f"
+    | Branch (con, s1, s2) -> if (testConditon con) then interpret s1 else interpret s2
+    | Repeat (i, s1) -> for j = 0 to i-1 do interpret s1
+
+//// Test the input expression
+//let x = Console.ReadLine() |> Input
+//let demoInput = Add( Const 5.0, x)
+//demoInput
+//|> evaluate
+//|> printfn "%.2f"
+//demoInput
+//|> formatExpression
+//|> printfn "%s"
+//    
+//// Test the condition union
+//let demoCon = Not (Equals( Const 5.0, x))
+//demoCon
+//|> testConditon
+//|> printfn "%b"
+
+
+let y = Repeat(3 , PrintStr "Hello World")
+interpret y
 // Ugly print the expression.
-let demo1 = Neg (Div (Pow (Const 2.0, Sqrt (Mult (Const 11.0, (Sub (Add (Const 10.0, Const 3.0), Const 2.0))))), Const 2.0))
-printfn "%O\n" demo1
+// let demo1 = Neg (Div (Pow (Const 2.0, Sqrt (Mult (Const 11.0, (Sub (Add (Const 10.0, Const 3.0), Const 2.0))))), Const 2.0))
+// printfn "%O\n" demo1
 
 // format and print the expression
-demo1
-|> formatExpression
-|> printfn "%s"
+// demo1
+// |> formatExpression
+// |> printfn "%s"
 
 // Evaluate and print the expression.
-demo1
-|> evaluate
-|> printfn "%.2f\n" // prints the result of evaluating 10 + 3 - 2
+// demo1
+// |> evaluate
+// |> printfn "%.2f\n" // prints the result of evaluating 10 + 3 - 2
 
 // a = 1, b = -5, c = 4
 // use our "language" to solve the quad formula
 
-printfn("For the quadratic function: x**2 - 5x + 4\nThe answers are: ")
+(*printfn("For the quadratic function: x**2 - 5x + 4\nThe answers are: ")
 let a = Const 1.0
 let b = Const -5.0
 let c = Const 4.0
@@ -76,7 +125,7 @@ minus
 |> printfn "%s"
 minus
 |> evaluate
-|> printfn "x = %.2f"
+|> printfn "x = %.2f"*)
 
 
 

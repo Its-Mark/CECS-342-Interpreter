bf605aa6f52d8d064d3e730d665ef24b0d94e1b4
Mark G <mark.garcia.8001@gmail.com>
2022-11-29 17:09:52 -0800

    push

--- HW4/hw4/Program.fs
+++ HW4/hw4/Program.fs
@@ -1,6 +1,7 @@
 open System
 open System.Runtime.InteropServices
 open Microsoft.FSharp.Collections
+open Microsoft.FSharp.Core
 
 
 // This is a very basic math interpreter, using unions to discriminate between 
@@ -20,6 +21,26 @@ type Expression =
     | Input of String
     | Mod of Expression * Expression
     | Var of String
+    
+type Condition =
+    | Equals of Expression * Expression
+    | And of Condition * Condition
+    | Or of Condition * Condition
+    | Not of Condition    
+type Statement =
+    | Noop
+    | PrintStr of String
+    | PrintExpr of Expression
+    | Branch of Condition * Statement list * Statement list 
+    | Repeat of int * Statement list
+    | Set of string * Expression
+    | While of Condition * Statement list
+    | Function of string * string list * Statement list
+    | Return of Expression
+       
+type NamedEntity =
+    | Variable of float
+    | Fxn of string list * Statement list
    
 // evaluate converts an Expression object into the floating-point number it represents.
 let rec evaluate expr state =
@@ -34,9 +55,10 @@ let rec evaluate expr state =
     | Pow (expr1, expr2) -> (evaluate expr1 state) ** (evaluate expr2 state)
     | Input s -> printf "%s" s; Console.ReadLine() |> float
     | Mod (expr1, expr2) -> (evaluate expr1 state) % (evaluate expr2 state)
-    | Var v -> state |> Map.find v
-    
-// format expression
+    | Var v -> state |> (Map.find v) 
+               
+
+//// format expression
 let rec formatExpression expr state=
     match expr with 
     | Const c -> sprintf "%.2f" c
@@ -48,59 +70,86 @@ let rec formatExpression expr state=
     | Sqrt expr1 -> sprintf "(sqrt(%s))" (formatExpression expr1 state)
     | Pow (expr1, expr2) -> sprintf "(%s ** %s)" (formatExpression expr1 state) (formatExpression expr2 state)
     | Input s -> sprintf "%.2f" (float s)
-    | Mod (expr1, expr2) -> sprintf "(%s mod %s)" (formatExpression expr1 state) (formatExpression expr2 state)
+    | Mod (expr1, expr2) -> sprintf "(%s %% %s)" (formatExpression expr1 state) (formatExpression expr2 state)
     | Var v -> sprintf "(%s = %f)" v (state |> Map.find v)
-type Condition =
-    | Equals of Expression * Expression
-    | And of Condition * Condition
-    | Or of Condition * Condition
-    | Not of Condition
+
 
 let rec testCondition con state=
     match con with
     | Equals (expr1, expr2) -> evaluate expr1 state = evaluate expr2 state
     | And (con1, con2) -> testCondition con1 state && testCondition con2 state
     | Or (con1, con2) -> testCondition con1 state || testCondition con2 state
-    | Not con1 -> testCondition con1 state |> not 
+    | Not con1 -> testCondition con1 state |> not
     
-type Statement =
-    | Noop
-    | PrintStr of String
-    | PrintExpr of Expression
-    | Branch of Condition * Statement * Statement
-    | Repeat of int * Statement
-    | Set of string * Expression
-    //| While of Condition * Statement list
-    
-let rec interpret state iState=
-    match state with
-    | Noop -> (); iState
-    | PrintStr str -> printfn "%s" str; iState
-    | PrintExpr expr -> (evaluate expr iState) |> printfn "%.2f"; iState
-    | Branch (con, sList1, sList2) -> if (testCondition con iState) then interpretProgram sList1 iState else interpretProgram sList2 iState |> ignore; iState
-    | Repeat (i, sList1) -> if i = 0 then interpret Noop iState else interpretProgram sList1 iState |> ignore; interpret (Repeat (i-1, sList1)) iState |> ignore; iState
-    | Set (v, expr) -> iState |> Map.add v (evaluate expr iState)
-    //| While (con, sList) -> if (testCondition con iState) then interpretPro; interpret (While (con, sList)) |> ignore; iState
-
+let rec interpret stmnt state=
+    match stmnt with
+    | Noop -> state
+    | PrintStr str -> printfn "%s" str; state
+    | PrintExpr expr -> (evaluate expr state) |> printfn "%.2f"; state
+    | Branch (con, sList1, sList2) -> if (testCondition con state) then interpretProgram sList1 state else interpretProgram sList2 state
+    | Repeat (i, sList1) -> if i = 0 then interpret Noop state else interpretProgram sList1 state |> ignore; interpret (Repeat (i-1, sList1)) state
+    | Set (v, expr) -> state |> Map.add v (evaluate expr state) //(Variable(evaluate expr state))
+    | While (con, sList) -> if (testCondition con state) then interpretProgram sList state |> interpret ( While (con, sList)) else state
+    // | Function (name, param, sList) -> Map.add name (Fxn (param, sList)) state  
+                                
 and interpretProgram statementList currentState =
+    let mutable currentState = currentState
     for item in statementList do
-        ans <- interpret item currentState
-        
+        currentState <- interpret item currentState
+    currentState
+
+//test part 1
+printfn "Testing 'Expressions'....."
+let state = Map.ofList [("x", 4.0)]  // hard-code a starting state of x=4.
+let expr = Add (Const 1.0, Var "x")
+evaluate expr state |> printfn "%f"
+
+//test part 2
+printfn "Testing statements....."
 let initialState = Map.ofList [("x", 4.0)]  // hard-code a starting state of x=4.
+let statement = PrintExpr (Add (Const 1.0, Var "x"))
+interpret statement initialState |> printfn "%O"
+
+//test part 3
+printfn "Testing 'Defining Variables'....."
+let initialState2 = Map.ofList [("x", 4.0)]  // hard-code a starting state of x=4.
+let statement2 = Set ("x", Add (Const 1.0, Var "x"))
+interpret statement initialState2 |> printfn "%O"
+
+//test part 4
+printfn "Testing 'Running a Program'....."
+let initialState3 = Map.ofList [("x", 4.0)]  // hard-code a starting state of x=4.
+let programResult =
+  initialState3
+  |> interpret (Set ("x", Add (Const 1.0, Var "x")))
+  |> interpret (PrintExpr (Var "x"))
+  |> interpret (Set ("x", Const 0.0))
+  |> interpret (PrintExpr (Var "x"))
+  |> printfn "%O"
+  
+//test part 5
+printfn "Testing with statement lists......."
+let initialState4 = Map.ofList [("x", 4.0)]  // hard-code a starting state of x=4.
 let myProgram = [
   Set ("x", Add (Const 1.0, Var "x"));
   PrintExpr (Var "x");
   Set ("x", Const 0.0);
   PrintExpr (Var "x")
 ]
-let programResult =
-  initialState
-  |> interpret (Set ("x", Add (Const 1.0, Var "x")))
-  |> interpret (PrintExpr (Var "x"))
-  |> interpret (Set ("x", Const 0.0))
-  |> interpret (PrintExpr (Var "x"))
-  |> printfn "%O"
+interpretProgram myProgram initialState4 |> ignore
 
+//test part 6
+printfn "Testing While Loop........"
+let initialState5 = Map.ofList [("x", 4.0)]  // hard-code a starting state of x=4.
+let myProgram2 = [
+  Set ("i", Const 0.0);
+  While (Not (Equals (Const 10.0, Var "i")), [PrintExpr (Var "i");Set ("i", (Add (Var "i", Const 1.0)))]);
+  PrintExpr (Mult (Const 2.0, Var "i"))
+]
+interpretProgram myProgram2 initialState5 |> ignore
+
+
+//interpretProgram myProgram initialState
 
 (*
 EVERYTHING BELOW THIS COMMENT WORKS
@@ -109,10 +158,6 @@ EVERYTHING BELOW THIS COMMENT WORKS
 
 let initialState = Map.ofList [("x", 4.0)]  // hard-code a starting state of x=4.
 let statement = Set ("x", Add (Const 1.0, Var "x"))
-interpret statement initialState |> printfn "%O"
-
-let initialState = Map.ofList [("x", 4.0)]  // hard-code a starting state of x=4.
-let statement = PrintExpr (Add (Const 1.0, Var "x"))
 interpret statement initialState |> printfn "%O"
 
 //let state = Map.ofList [("x", 4.0)]  // hard-code a starting state of x=4.
